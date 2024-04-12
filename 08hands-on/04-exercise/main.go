685cffc80e50ec1555f83caaa237166aded7f871
Diwakar <diwakar@cryptoforce.in>
2024-04-12 12:20:38 +0530

    Hands on - exercise 04

---
+++ 08hands-on/04-exercise/main.go
@@ -0,0 +1,27 @@
+package main
+
+// Starting with this code, pull the values off the channel
+// using a select statement
+
+import (
+	"fmt"
+)
+
+func main() {
+	q := make(chan int)
+	c := gen(q)
+
+	receive(c, q)
+
+	fmt.Println("about to exit")
+}
+
+func gen(q <-chan int) <-chan int {
+	c := make(chan int)
+
+	for i := 0; i < 100; i++ {
+		c <- i
+	}
+
+	return c
+}
