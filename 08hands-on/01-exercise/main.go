98bf74ff965ca3482d4fe313542d9bc3f0496037
Diwakar <diwakar@cryptoforce.in>
2024-04-12 11:14:25 +0530

    Deadlock! - Solved using buffered channel

--- 08hands-on/01-exercise/main.go
+++ 08hands-on/01-exercise/main.go
@@ -6,10 +6,11 @@ import "fmt"
 // ○ using func literal, aka, anonymous self-executing func
 // ○ a buffered channel
 func main() {
-	c := make(chan int)
-	go func() {
-		c <- 42
-	}()
+	c := make(chan int, 1)
+	// go func() {
+	// 	c <- 42
+	// }()
+	c <- 42
 
 	fmt.Println(<-c)
 }
