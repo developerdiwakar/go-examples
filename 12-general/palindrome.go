109f90b72121670561d192633e1abbe7ba9cc56b
diwakar kumar <developer.diwakar@gmail.com>
2024-06-21 15:42:47 +0530

    Palindrome - String and Number

---
+++ 12-general/palindrome.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+// Example 1: Golang Program code to Check whether a Number is palindrome or not
+func checkStringPalindrome(str string) bool {
+	count := len(str)
+	for i := 0; i < count/2; i++ {
+		if str[i] != str[count-1-i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Example 2: Golang Program Code to Check whether a String is palindrome or not
+func checkNumberPalindrome(number uint64) bool {
+	num := number
+	var reverseNum uint64 = 0
+	var a uint64
+	for {
+		a = num % 10
+		reverseNum = reverseNum*10 + a
+		num /= 10
+
+		if num == 0 {
+			break
+		}
+	}
+
+	if number == reverseNum {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func main() {
+	// Taking string input from user for palindrome check
+	var str string
+	fmt.Println("Enter a string: ")
+	_, err := fmt.Scanln(&str)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	if checkStringPalindrome(str) {
+		fmt.Println(str, "is Palindrome")
+	} else {
+		fmt.Println(str, "is not Palindrome")
+	}
+
+	var number uint64
+	fmt.Println("Enter a number: ")
+	_, err = fmt.Scanln(&number)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	if checkNumberPalindrome(number) {
+		fmt.Println(number, "is Palindrome")
+	} else {
+		fmt.Println(number, "is not Palindrome")
+	}
+
+}
