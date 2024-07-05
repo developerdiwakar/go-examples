ed5cca46690af0b760825794c2fe544b5f15cda6
diwakar kumar <developer.diwakar@gmail.com>
2024-07-05 22:34:26 +0530

    Fixed - input with spaces, remove special characters, changed to lowercase

--- 12-general/palindrome/main.go
+++ 12-general/palindrome/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
-
-	"github.com/gofiber/fiber/v2/log"
+	"os"
+	"strings"
+	"unicode"
 )
 
-// Example 1: Golang Program code to Check whether a Number is palindrome or not
+// Example 1: Golang Program code to Check whether a string is palindrome or not
 func checkStringPalindrome(str string) bool {
 	count := len(str)
 	for i := 0; i < count/2; i++ {
@@ -19,7 +21,7 @@ func checkStringPalindrome(str string) bool {
 	return true
 }
 
-// Example 2: Golang Program Code to Check whether a String is palindrome or not
+// Example 2: Golang Program Code to Check whether a Number is palindrome or not
 func checkNumberPalindrome(number uint64) bool {
 	num := number
 	var reverseNum uint64 = 0
@@ -42,15 +44,37 @@ func checkNumberPalindrome(number uint64) bool {
 
 }
 
+// removeSpecialCharacters removes non-alphanumeric characters from a string
+func removeSpecialCharacters(str string) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) || unicode.IsSpace(r) {
+			return r
+		}
+		return -1
+	}, str)
+}
+
 func main() {
 	// Taking string input from user for palindrome check
 	var str string
-	fmt.Println("Enter a string(without space): ")
-	_, err := fmt.Scanln(&str)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter a string:")
+
+	// Read the input until the newline character
+	input, err := reader.ReadString('\n')
 	if err != nil {
-		log.Fatalf("Error reading input: %v\n", err)
+		fmt.Println("Error reading input:", err)
 		return
 	}
+
+	// Trim the newline character from the end
+	str = strings.TrimSpace(input)
+	// Replace spaces with empty string
+	str = strings.ReplaceAll(str, " ", "")
+	// Convert to lower case
+	str = strings.ToLower(str)
+	// Remove special characters
+	str = removeSpecialCharacters(str)
 
 	if checkStringPalindrome(str) {
 		fmt.Println(str, "is Palindrome")
